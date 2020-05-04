d145bca0ac47e26917cc93473d23a99c5eabd114
2Echo <49248731+hanhao2006@users.noreply.github.com>
2020-05-04 17:36:55 -0400

    update sending message in group

--- app/src/main/java/com/example/whatappclone/GroupChatActivity.java
+++ app/src/main/java/com/example/whatappclone/GroupChatActivity.java
@@ -1,13 +1,29 @@
 package com.example.whatappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class GroupChatActivity extends AppCompatActivity {
 
@@ -17,19 +33,67 @@ public class GroupChatActivity extends AppCompatActivity {
     private ScrollView mScrollView;
     private TextView textViewDisplayMessage;
 
+    // get intent from group Fragment
+    private String currentGroupName, currentUserId, currentUserName, currentDate,currentTime;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference, databaseReferenceGroupName, groupMessageKeyRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat);
 
         Initialize();
+        // get intent from group Fragment
+        currentGroupName = getIntent().getExtras().get("groupName").toString();
+        Toast.makeText(this,currentGroupName,Toast.LENGTH_SHORT).show();
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
+        databaseReferenceGroupName = FirebaseDatabase.getInstance().getReference().child("Groups").child(currentGroupName);
+        GetUserInfo();
+        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SaveMessageInfoToDatabase();
+                editTextInputMessage.setText("");
+            }
+        });
+
+
     }
+
+
+
+    // get intent from group Fragment
+    private void GetUserInfo() {
+
+        databaseReference.child(currentUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    currentUserName = dataSnapshot.child("name").getValue().toString();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+
 
     private void Initialize() {
 
         toolbar = findViewById(R.id.Group_chat_layout);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Group Name");
+        getSupportActionBar().setTitle(currentGroupName);
 
         sendMessageButton = findViewById(R.id.btnSendMessage);
         editTextInputMessage = findViewById(R.id.input_group_message);
@@ -37,4 +101,39 @@ public class GroupChatActivity extends AppCompatActivity {
         mScrollView = findViewById(R.id.my_scroll_view);
 
     }
+
+
+    private void SaveMessageInfoToDatabase() {
+
+        String message = editTextInputMessage.getText().toString();
+        String messageId = databaseReferenceGroupName.push().getKey();
+
+        if(TextUtils.isEmpty(message)){
+            Toast.makeText(this, "Please Write message", Toast.LENGTH_SHORT).show();
+        }else{
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat currentDateFormat = new SimpleDateFormat("MMM dd,yyyy");
+            currentDate = currentDateFormat.format(calendar.getTime());
+
+            Calendar calendarTime = Calendar.getInstance();
+            SimpleDateFormat currentTimeFormat = new SimpleDateFormat("hh:mm a");
+            currentTime = currentTimeFormat.format(calendarTime.getTime());
+
+            HashMap<String,Object> groupMessageKey = new HashMap<>();
+            databaseReferenceGroupName.updateChildren(groupMessageKey);
+            groupMessageKeyRef = databaseReferenceGroupName.child(messageId);
+
+            HashMap<String,Object> messageInfo = new HashMap<>();
+            messageInfo.put("name",currentUserName);
+            messageInfo.put("message",message);
+            messageInfo.put("date",currentDate);
+            messageInfo.put("time",currentTime);
+            groupMessageKeyRef.updateChildren(messageInfo);
+            textViewDisplayMessage.setText(message);
+
+
+        }
+    }
 }
+
+
--- app/src/main/java/com/example/whatappclone/GroupsFragment.java
+++ app/src/main/java/com/example/whatappclone/GroupsFragment.java
@@ -1,5 +1,6 @@
 package com.example.whatappclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -50,6 +52,16 @@ public class GroupsFragment extends Fragment {
         databaseReference = FirebaseDatabase.getInstance().getReference("Groups");
         getGroup();
         Intialize();
+
+
+         // set group name to group chat activity
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String currentGroupName = parent.getItemAtPosition(position).toString();
+                startActivity(new Intent(getContext(),GroupChatActivity.class).putExtra("groupName",currentGroupName));
+            }
+        });
         return viewGroup;
     }
 
@@ -61,7 +73,6 @@ public class GroupsFragment extends Fragment {
     }
 
     private void getGroup() {
-
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -81,7 +92,10 @@ public class GroupsFragment extends Fragment {
 
             }
         });
-
     }
+
+
+
+
 
 }
